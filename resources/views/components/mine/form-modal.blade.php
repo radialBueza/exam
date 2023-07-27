12f1947f6b56cb8e0ba1d981cc010e4b33a9edbd
radialBueza <52782202+radialBueza@users.noreply.github.com>
2023-07-27 18:26:16 +0800

    Cont.

--- resources/views/components/mine/form-modal.blade.php
+++ resources/views/components/mine/form-modal.blade.php
@@ -1,3 +1,4 @@
+@props(['title', 'subtitle', 'id'])
 <div x-data="{
     showForm: true,
     confirm: false,
@@ -77,5 +78,28 @@
         confirm = false
     }
 })">
-    {{$slot}}
+<div>
+    <div class="flex items-center justify-between space-x-4">
+        <h3 class="text-xl font-medium text-gray-800 ">{{ucwords($title)}}</h3>
+        <button @click="openAdd = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+        </button>
+    </div>
+    <p class="mt-2 text-sm text-gray-500 ">
+        {{ucfirst($subtitle)}}
+    </p>
+    <form class="mt-5" id="{{$id}}" @submit.prevent="await sendData($el)">
+        <div>
+            {{$slot}}
+        </div>
+        <div class="flex justify-end mt-6">
+            <button type="submit" class="bg-green-500 focus:ring-green-700 inline-flex items-center px-4 py-2 border-transparent rounded-md font-semibold text-xs text-white tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150">
+                Add Department
+            </button>
+        </div>
+    </form>
+</div>
+
 </div>
--- resources/views/components/mine/text-input.blade.php
+++ resources/views/components/mine/text-input.blade.php
@@ -2,7 +2,7 @@
 
 <label for="{{$name}}" class="block text-sm text-gray-700 capitalize">{{ucwords($title)}} </label>
 <input id="{{$name}}" name="{{$name}}" placeholder="Enter {{ucfirst($title)}} {{ucfirst($name)}}. . . " type="text" class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md"
-    @keyup="validate" :class="error.{{$name}}?.msg && 'border-red-800 ring-1 ring-red-800 focus:ring-red-800 focus:border-red-800'" x-ref="{{$name}}"
+    @keyup="validate" :class="error.{{$name}}?.msg && 'border-red-800 ring-1 ring-red-800 focus:ring-red-800 focus:border-red-800'"
 >
 <div x-show="error.{{$name}}?.msg" x-text="error.{{$name}}?.msg" class="text-red-800 text-sm font-semibold my-2">
 </div>
