c9a33c70a11efccbbe3be7f0dcf550c44b5137c5
radialBueza <52782202+radialBueza@users.noreply.github.com>
2024-10-22 14:59:38 +0800

    Date and class inheritance
    Fix date and class inheritance is changed.

--- resources/views/components/mine/td-cell-primary.blade.php
+++ resources/views/components/mine/td-cell-primary.blade.php
@@ -6,7 +6,7 @@
     {{$slot}}
 </td> --}}
 @php
-    $class = ($isLink) ? 'text-ellipsis overflow-hidden px-6 py-4 font-medium text-gray-900 whitespace-nowrap capitalize hover:underline hover:text-gray-700' : 'text-ellipsis overflow-hidden px-6 py-4 font-medium text-gray-900 whitespace-nowrap capitalize'
+    $class = ($isLink) ? 'px-6 py-4 font-medium text-gray-900 whitespace-nowrap capitalize hover:underline hover:text-gray-700' : 'px-6 py-4 font-medium text-gray-900 whitespace-nowrap capitalize'
 @endphp
 <td {{ $attributes->merge(['class' => "{$class}"])}}>
     {{$slot}}
