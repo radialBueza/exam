38182e18e57fda1f1af3deb7b5501eb5ee42dc07
radialBueza <52782202+radialBueza@users.noreply.github.com>
2025-07-17 15:30:25 +0800

    Enforce https
    Enfroce https, so that https is no longer accessible

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
         JsonResource::withoutWrapping();
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
